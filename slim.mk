9d9ea0d31eaea86dc9dd839f3f82e6992bb1da45
Tylor Janzen <tylor.janzen@gmail.com>
2014-02-06 17:09:59 -0700

    Fix compile error. Missing ) in slim.mk

--- slim.mk
+++ slim.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/celoxhd/full_celoxhd.mk
+$(call inherit-product, device/samsung/celoxhd/full_celoxhd.mk)
 
 # Inherit some common slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
